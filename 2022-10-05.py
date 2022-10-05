e99e967e449d28b92e2fd85d36ba1d0d3837fc27
Eliashagge1 <59806524+Eliashagge1@users.noreply.github.com>
2022-10-05 16:43:49 +0200

    Update 2022-10-05.py

--- 2022-10-05.py
+++ 2022-10-05.py
@@ -58,28 +58,7 @@ musicVolume = standardVolume
 SFXVolume = standardVolume
 
 
-#For the saving
-class MyClass():
-    def __init__(self, param):
-        self.param = param
-def save_object(obj):
-    try:
-        with open("savefiles/data.pickle", "wb") as f:
-            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
-    except Exception as ex:
-        print("Failure to save", ex)
-def save_saved(obj):
-    try:
-        with open("savefiles/saved.pickle", "wb") as f:
-            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
-    except Exception as ex:
-        print("Failure to save", ex)
-def load_object(filename):
-    try:
-        with open(filename, "rb") as f:
-            return pickle.load(f)
-    except Exception as ex:
-        print("Failure to save", ex)
+
 
 
 #Start loadingscreen
