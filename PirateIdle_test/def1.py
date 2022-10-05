f3ddb7666ecaeb49b434b2f8a22efd49418ced69
lolex500 <113016864+lolex500@users.noreply.github.com>
2022-10-05 10:10:30 +0200

    Update and rename FPSdef.py to def1.py

--- PirateIdle_test/def1.py
+++ PirateIdle_test/def1.py
@@ -1,11 +1,8 @@
 import pygame, sys, time
 pygame.init()
 
-#Init def (8) and creates fonst
 
-
-
-#FPS counter start
+# FPS counter start
 def init_screen_and_clock():
     global screen, display, clock
     pygame.init()
@@ -13,30 +10,45 @@ def init_screen_and_clock():
     pygame.display.set_caption('Game')
     screen = pygame.display.set_mode(windowSize, 0, 32)
     clock = pygame.time.Clock()
- 
-#Fonts
+
+
+# Fonts
 def create_fonts(font_sizes_list):
-    #Creates different fonts with one list
+    # Creates different fonts with one list
     fonts = []
     for size in font_sizes_list:
         fonts.append(
             pygame.font.SysFont("Arial", size))
     return fonts
- 
-#Render display_fps
+
+
+# Render display_fps
 def render(fnt, what, color, where):
-    #Renders the fonts as passed from display_fps
+    # Renders the fonts as passed from display_fps
     text_to_show = fnt.render(what, 0, pygame.Color(color))
     screen.blit(text_to_show, where)
- 
- #Displays FPS
+
+
+# Displays FPS
 def display_fps():
-    #Data that will be rendered and blitted in _display
+    # Data that will be rendered and blitted in _display
     render(
-        fonts[0],
+        fonts[32],
         what=str(int(clock.get_fps())),
         color="white",
         where=(0, 0))
 
+
+# Init def (8) and creates fonts
 init_screen_and_clock()
-fonts = create_fonts([32, 16, 14, 8])
+fonts = create_fonts([32, 16, 14, 11, 8])
+
+
+# Scaling
+def transformScaleKeepRatio(image, size):
+    iwidth, iheight = image.get_size()
+    scale = min(size[0] / iwidth, size[1] / iheight)
+    new_size = (round(iwidth * scale), round(iheight * scale))
+    scaled_image = pygame.transform.smoothscale(image, new_size)
+    image_rect = scaled_image.get_rect(center = (size[0] // 2, size[1] // 2))
+    return scaled_image, image_rect
